cce09d16ff4b4bb4f177e31b85c79cf747f36948
Nguyễn Quốc Tuấn Lâm <lam.nguyen0612@hcmut.edu.vn>
2024-09-13 19:56:57 +0700

    hoan thanh ex9

--- stm_code/Core/Src/main.c
+++ stm_code/Core/Src/main.c
@@ -54,7 +54,46 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void clearNumberOnClock(int num){
+    switch (num) {
+    case 1:
+        HAL_GPIO_WritePin(LED_1_GPIO_Port,LED_1_Pin,GPIO_PIN_RESET);
+        break;
+    case 2:
+        HAL_GPIO_WritePin(LED_2_GPIO_Port,LED_2_Pin,GPIO_PIN_RESET);
+        break;
+    case 3:
+        HAL_GPIO_WritePin(LED_3_GPIO_Port,LED_3_Pin,GPIO_PIN_RESET);
+        break;
+    case 4:
+        HAL_GPIO_WritePin(LED_4_GPIO_Port,LED_4_Pin,GPIO_PIN_RESET);
+        break;
+    case 5:
+        HAL_GPIO_WritePin(LED_5_GPIO_Port,LED_5_Pin,GPIO_PIN_RESET);
+        break;
+    case 6:
+        HAL_GPIO_WritePin(LED_6_GPIO_Port,LED_6_Pin,GPIO_PIN_RESET);
+        break;
+    case 7:
+        HAL_GPIO_WritePin(LED_7_GPIO_Port,LED_7_Pin,GPIO_PIN_RESET);
+        break;
+    case 8:
+        HAL_GPIO_WritePin(LED_8_GPIO_Port,LED_8_Pin,GPIO_PIN_RESET);
+        break;
+    case 9:
+        HAL_GPIO_WritePin(LED_9_GPIO_Port,LED_9_Pin,GPIO_PIN_RESET);
+        break;
+    case 10:
+        HAL_GPIO_WritePin(LED_10_GPIO_Port,LED_10_Pin,GPIO_PIN_RESET);
+        break;
+    case 11:
+        HAL_GPIO_WritePin(LED_11_GPIO_Port,LED_11_Pin,GPIO_PIN_RESET);
+        break;
+    case 12:
+    	HAL_GPIO_WritePin(LED_12_GPIO_Port,LED_12_Pin,GPIO_PIN_RESET);
+    	break;
+    }
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,8 +131,10 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int counter =10;
   while (1)
   {
+	  clearNumberOnClock(counter);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
